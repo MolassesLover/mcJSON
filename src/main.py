38ee58569de376a75d1115bf0c40af471c478ccb
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-11-02 22:48:15 +0100

    Wrote pseudocode

--- src/main.py
+++ src/main.py
@@ -8,6 +8,17 @@ import subprocess
 
 
 def main():
+    # Parse arguments
+
+    # Open each .json file in the json/ directory
+
+    # Find and replace the $NAMESPACE and $SOURCE_BLOCK variables
+
+    # Save the generated string as a new `.json` file...
+    # ... with the appropriate file prefix...
+    # I.E $SOURCE_BLOCK, generating...
+    # ... $SOURCE_BLOCK_block.json.
+
     pass
 
 
