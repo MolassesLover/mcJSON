6afb438d82090af8284606e97976d493f732381d
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-11-02 23:08:55 +0100

    Moved model `.json` and added variable replacement

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # region Modules
 
+import argparse
 import subprocess
+import os
 
 # endregion
 
@@ -9,10 +11,24 @@ import subprocess
 
 def main():
     # Parse arguments
+    argumentParser = argparse.ArgumentParser()
+    argumentParser.add_argument("--namespace", type=str)
+    argumentParser.add_argument("--source_block", type=str)
+    arguments = argumentParser.parse_args()
 
-    # Open each .json file in the json/ directory
+    jsonPath: str = "json"
 
-    # Find and replace the $NAMESPACE and $SOURCE_BLOCK variables
+    # Open each `.json` file in the `json/` directory
+    for root, dirs, files in os.walk(jsonPath):
+        for file in files:
+            if file.lower().endswith("json".lower()):
+                # Find and replace the $NAMESPACE and $SOURCE_BLOCK variables
+                with open(os.path.join(root, file), "r") as newFile:
+                    filedata = newFile.read()
+
+                filedata = filedata.replace("$NAMESPACE", arguments.namespace)
+                filedata = filedata.replace("$SOURCE_BLOCK", arguments.source_block)
+                print(filedata)
 
     # Save the generated string as a new `.json` file...
     # ... with the appropriate file prefix...
